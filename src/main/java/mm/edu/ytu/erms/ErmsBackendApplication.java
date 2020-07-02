9c3fa9547feb31ecb2257dac7225c8c4c29948f0
Sai Han <thesaihan@gmail.com>
2020-07-03 00:02:12 +0630

    Change Application name

--- src/main/java/mm/edu/ytu/erms/ErmsBackendApplication.java
+++ src/main/java/mm/edu/ytu/erms/ErmsBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErmsBeApplication {
+public class ErmsBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ErmsBeApplication.class, args);
+		SpringApplication.run(ErmsBackendApplication.class, args);
 	}
 
 }
