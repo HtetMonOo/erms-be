b7f56c6426cfafedac0b3f75e7962042b383ef9d
Chaw Chit <chawchitsuthwe3699@gmail.com>
2020-07-06 13:07:21 +0630

    Update department repository

--- src/main/java/mm/edu/ytu/erms/repository/DepartmentRepository.java
+++ src/main/java/mm/edu/ytu/erms/repository/DepartmentRepository.java
@@ -3,7 +3,9 @@ package mm.edu.ytu.erms.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mm.edu.ytu.erms.model.Department;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, String> {
   
 }
