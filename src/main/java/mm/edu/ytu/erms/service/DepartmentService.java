608fa3d5c125c18ab46187f34ac7035d0adde977
Chaw Chit <chawchitsuthwe3699@gmail.com>
2020-07-06 13:08:03 +0630

    Update DepartmentService

--- src/main/java/mm/edu/ytu/erms/service/DepartmentService.java
+++ src/main/java/mm/edu/ytu/erms/service/DepartmentService.java
@@ -1,5 +1,14 @@
 package mm.edu.ytu.erms.service;
 
+import mm.edu.ytu.erms.model.Department;
+
+import java.util.List;
+
 public interface DepartmentService {
-  
+
+    List<Department> getAll();
+    Department getByCode(String code);
+    Department save(Department department);
+    Department update(Department department);
+    void deleteByCode(String code);
 }
