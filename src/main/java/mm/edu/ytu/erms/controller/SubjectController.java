ba5e160213fad2ff11b555473ce08578e0d877f3
Sai Han <thesaihan@gmail.com>
2020-07-17 00:38:56 +0630

    Remove unused imports

--- src/main/java/mm/edu/ytu/erms/controller/SubjectController.java
+++ src/main/java/mm/edu/ytu/erms/controller/SubjectController.java
@@ -3,12 +3,6 @@ package mm.edu.ytu.erms.controller;
 import mm.edu.ytu.erms.model.Subject;
 import mm.edu.ytu.erms.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
